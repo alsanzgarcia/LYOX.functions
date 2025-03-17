398c77bd2b6f564165fcedc5e21e759e80940be0
alsanzgarcia <asanz1234@gmail.com>
2025-03-17 09:51:01 +0100

    added hello.R dummy function

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+# Create and document the function
+#' Print Hello
+#'
+#' Just prints hello
+#'
+#' @param x          Variable that does not affect result
+#' @param return      
+#' @param examples
+#'
+#'@export
+
+hello <- function(x = 1){
+  return(paste0("Hello"))
+}
